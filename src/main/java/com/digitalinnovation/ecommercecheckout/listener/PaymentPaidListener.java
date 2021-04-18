cb6211950dbea15a49e444349212c5f09a730323
Andressa <andressa.olimpia2014@gmail.com>
2021-04-18 06:29:18 -0300

    correcao MessageConversionException

--- src/main/java/com/digitalinnovation/ecommercecheckout/listener/PaymentPaidListener.java
+++ src/main/java/com/digitalinnovation/ecommercecheckout/listener/PaymentPaidListener.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PaymentPaidListener {
 
-    private CheckoutRepository checkoutRepository;
+    private final CheckoutRepository checkoutRepository;
 
     @StreamListener(PaymentPaidSink.INPUT)
     public void handler(PaymentCreatedEvent event) {
--- src/main/java/com/digitalinnovation/ecommercecheckout/model/Checkout.java
+++ src/main/java/com/digitalinnovation/ecommercecheckout/model/Checkout.java
@@ -1,13 +1,17 @@
 package com.digitalinnovation.ecommercecheckout.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Builder
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Checkout {
 
     @Id
